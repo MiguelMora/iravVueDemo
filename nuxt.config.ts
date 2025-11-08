8ed44cf2b79fa49a3e2e0138454e06fe236254c9
Miguel Ángel Mora <miguel.mora@uam.es>
2025-11-08 19:42:42 +0100

    migrada estructura de directorios a nuxt 4 usando modulos de nuxt en lugar de plugins reemplazado lodash por es-toolkit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: [
-    'vuetify/lib/styles/main.sass',
-    '@mdi/font/css/materialdesignicons.min.css',
-  ],
-  build: {
-    transpile: ['vuetify'],
-  },
-  vite: {
-    define: {
-      'process.env.DEBUG': false,
+    compatibilityDate: '2025-11-08',
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
     },
-  },
-  modules: ['@pinia/nuxt'],
-  ssr: false,
-  runtimeConfig: {
-    // Override it with environment variables starting with NUXT, _ as separator
-    // except public ones, these variables can only be read from the server
-    IsServer: true, // to detect if in the server side or not, because the browser can not read it
-    public: {
-      // public constants can be universally accessed, in both browser and server
+    modules: [
+        '@nuxt/eslint',
+        'vuetify-nuxt-module',
+        '@pinia/nuxt',
+        '@vite-pwa/nuxt',
+    ],
+    ssr: false,
+    eslint: {
+        config: {
+            standalone: false,
+        },
     },
-  },
+    vuetify: {
+        vuetifyOptions: {
+            labComponents: true,
+        },
+    },
+    runtimeConfig: {
+        // Override it with environment variables starting with NUXT, _ as separator
+        // except public ones, these variables can only be read from the server
+        public: {
+            // public constants can be universally accessed, in both browser and server
+        },
+    },
 })
